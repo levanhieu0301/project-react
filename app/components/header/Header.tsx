98220d84d93c9455f3eb4369939282cb41ecd0d5
lvh0301 <levanhieu0301@gmail.com>
2026-06-26 18:55:46 +0700

    Hiển thị Header đúng thông tin

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -14,19 +14,19 @@ export const Header= () => {
   const handleShowMenu = () => {
     setShowMenu(!showMenu)
   }
-  const handleLogout = () => {
+  const handleLogout = (linkRedirect: string) => {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/logout`, {
       credentials: "include"
     })
     .then(res => res.json())
     .then((data) => {
       if(data.code == "success"){
-        router.push("/user/login")
+        router.push(linkRedirect)
       }
     })
 
   }
-  const {isLogin, infoUser} = AuthenToken();
+  const {isLogin, infoUser, infoCompany} = AuthenToken();
   return (
     <>
       <header className="header bg-[#000071] py-[15px]">
@@ -45,23 +45,53 @@ export const Header= () => {
             >
               {isLogin? (
                 <>
-                  {/* Đã đăng nhập  */}
-                  <Link href="#" className="">{infoUser.fullName}</Link>
-                  <ul className="absolute top-[100%] right-[0px] w-[200px] bg-[#000065] hidden group-hover/sub-1:block">
-                    <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
-                      <Link href="" className="text-white font-[600] text-[16px]">
-                        Thông tin cá nhân
-                      </Link>
-                    </li>
-                    <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
-                      <Link href="" className="text-white font-[600] text-[16px]">
-                        Quản lý CV đã gửi
-                      </Link>
-                    </li>
-                    <li onClick={handleLogout} className="py-[10px] cursor-pointer px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
-                        Đăng xuất
-                    </li>
-                  </ul>
+                  {/* Đã đăng nhập user */}
+                  {infoUser && (
+                    <>
+                      <Link href="#" className="">{infoUser.fullName}</Link>
+                      <ul className="absolute top-[100%] right-[0px] w-[200px] bg-[#000065] hidden group-hover/sub-1:block">
+                        <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                          <Link href="" className="text-white font-[600] text-[16px]">
+                            Thông tin cá nhân
+                          </Link>
+                        </li>
+                        <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                          <Link href="" className="text-white font-[600] text-[16px]">
+                            Quản lý CV đã gửi
+                          </Link>
+                        </li>
+                        <li onClick={() => handleLogout("/user/login")} className="py-[10px] cursor-pointer px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                            Đăng xuất
+                        </li>
+                      </ul>
+                    </>
+                  )}
+                  {/* Đã đăng nhập company */}
+                  {infoCompany && (
+                    <>
+                      <Link href="#" className="">{infoCompany.companyName}</Link>
+                      <ul className="absolute top-[100%] right-[0px] w-[200px] bg-[#000065] hidden group-hover/sub-1:block">
+                        <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                          <Link href="" className="text-white font-[600] text-[16px]">
+                            Thông tin công ty
+                          </Link>
+                        </li>
+                        <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                          <Link href="" className="text-white font-[600] text-[16px]">
+                            Quản lý CV
+                          </Link>
+                        </li>
+                        <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                          <Link href="" className="text-white font-[600] text-[16px]">
+                            Quản lý việc làm
+                          </Link>
+                        </li>
+                        <li onClick={() => handleLogout("/company/login")} className="py-[10px] cursor-pointer px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                            Đăng xuất
+                        </li>
+                      </ul>
+                    </>
+                  )}
                 </>
               ): (
                 <>
--- hooks/useAuthen.tsx
+++ hooks/useAuthen.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 export const AuthenToken = () => {
   const [isLogin, setIsLogin] = useState(false)
   const [infoUser, setInfoUser] = useState<any>(null);
+  const [infoCompany, setInfoCompany] = useState<any>(null);
   const pathname = usePathname()
   useEffect(()=> {
      fetch(`${process.env.NEXT_PUBLIC_API_URL}/authen/check`, {
@@ -17,7 +18,15 @@ export const AuthenToken = () => {
       }
       if(data.code == "success"){
           setIsLogin(true);
-          setInfoUser(data.infoUser);
+          if(data.infoUser){
+            setInfoUser(data.infoUser);
+            setInfoCompany(null)
+          }
+          if(data.infoCompany){
+            setInfoCompany(data.infoCompany)
+            setInfoUser(null);
+          }
+
       }
     })
   },[pathname])
@@ -25,6 +34,7 @@ export const AuthenToken = () => {
 
   return {
     isLogin: isLogin,
-    infoUser: infoUser
+    infoUser: infoUser,
+    infoCompany: infoCompany
   }
 }
