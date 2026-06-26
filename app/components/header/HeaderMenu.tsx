7afd2063a4f0190a011ae670f3627c4ff77f9cab
lvh0301 <levanhieu0301@gmail.com>
2026-06-26 10:40:49 +0700

    Tính năng Đăng ký (Nhà tuyển dụng)-FE

--- app/components/header/HeaderMenu.tsx
+++ app/components/header/HeaderMenu.tsx
@@ -54,11 +54,23 @@ export const HeaderMenu = (props: {
             </Link>
             <FaAngleDown className="text-white text-[16px]" />
           </li>
-          <li className="inline-flex lg:w-auto w-full items-center lg:justify-start justify-between lg:p-0 p-[10px] gap-x-[8px]">
+          <li className="inline-flex lg:w-auto w-full items-center lg:justify-start justify-between lg:p-0 p-[10px] gap-x-[8px] relative group/sub-1">
             <Link href="#" className="font-[600] text-[16px] text-white">
               Nhà Tuyển Dụng
             </Link>
             <FaAngleDown className="text-white text-[16px]" />
+            <ul className="absolute top-[100%] left-[0] w-[280px] bg-[#000065] hidden group-hover/sub-1:block">
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096] relative group/sub-2">
+                <Link href="/company/login" className="font-[600] text-[16px] text-white">
+                  Đăng nhập
+                </Link>
+              </li>
+              <li className="py-[10px] px-[16px] rounded-[4px] flex items-center justify-between hover:bg-[#000096]">
+                <Link href="/company/register" className="font-[600] text-[16px] text-white">
+                  Đăng ký
+                </Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
