030052c5a604d28f21cbf4106ce38d7f84892817
lvh0301 <levanhieu0301@gmail.com>
2026-06-30 19:17:56 +0700

    Trang thông tin công ty (Nhà tuyển dụng) - FE

---
+++ app/(pages)/company-manage/profile/FormProfileCompany.tsx
@@ -0,0 +1,271 @@
+/* eslint-disable react-hooks/set-state-in-effect */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client"
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import { FilePond, registerPlugin} from 'react-filepond'
+import 'filepond/dist/filepond.min.css'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+import { useEffect, useState } from 'react';
+registerPlugin(FilePondPluginFileValidateType, FilePondPluginImagePreview);
+import JustValidate from 'just-validate';
+import { AuthenToken } from '@/hooks/useAuthen';
+import { Toaster, toast } from 'sonner'
+
+
+export const FormProfileCompany = () => {
+  const {infoCompany} = AuthenToken();
+  const [cities, setCities] = useState<any>([])
+  const [avatars,setAvatars] = useState<any[]>([])
+  const [isValid, setIsValid] = useState(false)
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/company-manage/cities`)
+      .then(res => res.json())
+      .then(data => {
+        if(data.code == "error"){
+          setCities([])
+        }
+        if(data.code == "success"){
+          setCities(data.cityList)
+        }
+      })
+  }, [])
+  useEffect(() => {
+    if(infoCompany){
+      if(infoCompany.avatar){
+        setAvatars([
+          {
+            source: infoCompany.avatar
+          }
+        ])
+      }
+      const validator = new JustValidate("#formProfileCompanyManage");
+
+      validator
+        .addField('#companyName', [
+          {
+            rule: 'required',
+            errorMessage: 'Vui lòng nhập tên công ty!'
+          },
+          {
+            rule: 'maxLength',
+            value: 200,
+            errorMessage: 'Tên công ty không được vượt quá 200 ký tự!',
+          },
+        ])
+        .addField('#email', [
+          {
+            rule: 'required',
+            errorMessage: 'Vui lòng nhập email của bạn!',
+          },
+          {
+            rule: 'email',
+            errorMessage: 'Email không đúng định dạng!',
+          },
+        ])
+        .onFail(() => {
+          setIsValid(false)
+        })
+        .onSuccess(() => {
+          setIsValid(true)
+        });
+    }
+    }, [infoCompany])
+  const handleSubmit = (event: any) => {
+    if(isValid == true){
+      const companyName = event.target.companyName.value;
+      const city = event.target.city.value;
+      const address = event.target.address.value;
+      const companyModel = event.target.companyModel.value;
+      const companyEmployees = event.target.companyEmployees.value;
+      const workingTime = event.target.workingTime.value;
+      const workOverTime = event.target.workOverTime.value;
+      const email = event.target.email.value;
+      const phone = event.target.phone.value;
+      const description = event.target.description.value;
+      let avatar = null
+      if(avatars.length > 0){
+        avatar = avatars[0].file
+      }
+
+      const formData = new FormData()
+      formData.append("companyName", companyName);
+      formData.append("city", city);
+      formData.append("address", address);
+      formData.append("companyModel", companyModel);
+      formData.append("companyEmployees", companyEmployees);
+      formData.append("workingTime", workingTime);
+      formData.append("workOverTime", workOverTime);
+      formData.append("email", email);
+      formData.append("phone", phone);
+      formData.append("description", description);
+      formData.append("avatar", avatar);
+      console.log("Chạy vào đây")
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/company-manage/profile`, {
+        method: "PATCH",
+        body: formData, 
+        credentials: "include"
+      })
+      .then(res => res.json())
+      .then(data => {
+         if(data.code == "error") {
+          toast.error(data.message);
+        }
+        if(data.code == "success") {
+          toast.success(data.message);
+        }
+      })
+    }
+  }
+  return (
+    <>
+      <Toaster richColors position="top-right"/>
+      {infoCompany && (
+        <form action="" id ="formProfileCompanyManage" onSubmit={handleSubmit} className="grid sm:grid-cols-2 grid-cols-1 gap-x-[20px] gap-y-[15px]">
+          <div className="sm:col-span-2">
+            <label htmlFor="companyName" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Tên công ty *
+            </label>
+            <input 
+              type="text" 
+              name="companyName" 
+              defaultValue={infoCompany.companyName}
+              id="companyName" 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black" 
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="logo" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Logo
+            </label>
+            <FilePond
+              allowMultiple={false}
+              name="avatar"
+              labelIdle="+"
+              allowRemove={true}
+              acceptedFileTypes={['image/*']}
+              allowImagePreview={true}
+              files={avatars}
+              onupdatefiles={setAvatars}
+            />
+          </div>
+          <div className="">
+            <label htmlFor="city" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Thành phố
+            </label>
+            <select 
+              name="city" 
+              id="city" 
+              defaultValue={infoCompany.city}
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            >
+              {cities.map((item: any) =>(
+                <option  key = {item._id} value={item._id}>{item.name}</option> 
+              ))}
+            </select>
+          </div>
+          <div className="">
+            <label htmlFor="address" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Địa chỉ
+            </label>
+            <input 
+              type="text" 
+              name="address"
+              defaultValue={infoCompany.address} 
+              id="address" 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            />
+          </div>
+          <div className="">
+            <label htmlFor="companyModel" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Mô hình công ty
+            </label>
+            <input 
+              type="text" 
+              name="companyModel" 
+              defaultValue={infoCompany.companyModel} 
+              id="companyModel" 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            />
+          </div>
+          <div className="">
+            <label htmlFor="companyEmployees" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Quy mô công ty
+            </label>
+            <input 
+              type="text" 
+              name="companyEmployees" 
+              id="companyEmployees" 
+              defaultValue={infoCompany.companyEmployees} 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            />
+          </div>
+          <div className="">
+            <label htmlFor="workingTime" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Thời gian làm việc
+            </label>
+            <input 
+              type="text" 
+              name="workingTime" 
+              defaultValue={infoCompany.workingTime} 
+              id="workingTime" 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            />
+          </div>
+          <div className="">
+            <label htmlFor="WorkOvertime" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Làm việc ngoài giờ
+            </label>
+            <input 
+              type="text" 
+              name="workOverTime" 
+              defaultValue={infoCompany.workOverTime} 
+              id="WorkOvertime" 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            />
+          </div>
+          <div className="">
+            <label htmlFor="email" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Email *
+            </label>
+            <input 
+              type="email" 
+              name="email" 
+              defaultValue={infoCompany.email} 
+              id="email" 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            />
+          </div>
+          <div className="">
+            <label htmlFor="phone" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Số điện thoại
+            </label>
+            <input 
+              type="text" 
+              name="phone" 
+              defaultValue={infoCompany.phone} 
+              id="phone" 
+              className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label htmlFor="description" className="block font-[500] text-[14px] text-black mb-[5px]">
+              Mô tả chi tiết
+            </label>
+            <textarea 
+              name="description" 
+              id="description" 
+              defaultValue={infoCompany.description} 
+              className="w-[100%] h-[350px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+            ></textarea>
+          </div>
+          <div className="sm:col-span-2">
+            <button className="bg-[#0088FF] rounded-[4px] h-[48px] px-[20px] font-[700] text-[16px] text-white">
+              Cập nhật
+            </button>
+          </div>
+        </form>
+      )}
+    </>
+  )
+}
