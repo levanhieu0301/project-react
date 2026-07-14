9cac1bb79b3198be8cc714d5d42a75a9b1e84560
lvh0301 <levanhieu0301@gmail.com>
2026-07-14 15:22:20 +0700

    Trang quản lý CV (Nhà tuyển dụng)-FE

---
+++ config/cvList.tsx
@@ -0,0 +1,17 @@
+export const cvStatusList = [
+  {
+    label: "Chưa duyệt",
+    value: "initial",
+    color: "#121212"
+  },
+  {
+    label: "Đã duyệt",
+    value: "approved",
+    color: "#47BE02"
+  },
+  {
+    label: "Từ chối",
+    value: "rejected",
+    color: "#FF5100"
+  }
+];
